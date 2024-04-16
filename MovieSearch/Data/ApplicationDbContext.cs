901f7e6e20c8e2013dfb1b5bb8338eb3701beb0a
VitaliyBondar <ipz234_bvo@student.ztu.edu.ua>
2024-04-16 22:13:37 +0300

    Added some new changes(like a access to add review;beautiful design and you can see all reviews

---
+++ MovieSearch/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MovieSearch.Models;
+
+namespace MovieSearch.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { 
+        }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(new Genre { Id = 1, Name = "Action" },
+                new Genre { Id = 2, Name = "Adventure" },
+                new Genre { Id = 3, Name = "Animation" });
+            modelBuilder.Entity<Review>().HasData(new Review
+            {
+                Id = 1, Headline = "Beautiful Landscapes",
+                TextReview =
+                    "If that makes any sense. What I'm trying to say while pointing Aristotle's quote into a mirror, is that this is worth watching simply for all of the outstanding individual performances. There are many other reasons to tune in, but the acting clinic on parade here is a lot of fun.",
+                SpoilersConsist = false
+            });
+        }
+    }
+}
+    
---
+++ MovieSearch/Migrations/20240404131854_ChangeProperiesInTableToEf.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieSearch.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeProperiesInTableToEf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsShort",
+                table: "Genres");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShort",
+                table: "Genres",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
